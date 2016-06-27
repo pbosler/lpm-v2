3d60475f968102ab4b97ac06e1ac6b7defaf3250
Peter Bosler <pabosle@sandia.gov>
2016-06-27 11:52:18 -0600

    changed divergent test case to gaussian hills tracer

--- runDivWindDirect.sh
+++ runDivWindDirect.sh
@@ -3,14 +3,14 @@
 #SBATCH --nodes=1
 #SBATCH --time=0:60:00
 #SBATCH --account=fy150039
-#SBATCH --job-name=adv-divWindCosBells
+#SBATCH --job-name=adv-gHills-lpmD
 #SBATCH --partition=ec
 #SBATCH --mail-type=ALL
 
 LPM_ROOT=$HOME/lpm-v2
 #OUTPUT_LOC=$HOME/modelData/transport
 OUTPUT_LOC=/fscratch/pabosle/modelData/transport
-JOB_FILENAME=divWindCosBellsOutput.txt
+JOB_FILENAME=divWindGHillsOutput.txt
 
 cd $LPM_ROOT/build
 
@@ -18,28 +18,28 @@ rm -f $JOB_FILENAME
 
 for i in `seq 1 7`
 do
-cat <<EOF > divWindCosineBells.namelist
+cat <<EOF > divWindGHills.namelist
 &meshDefine
 	faceKind = 3 
 	initNest = ${i}
 	amrLimit = 0
-/
+/case 
 
 &timestepping
 	dt = 0.0125
 	tfinal = 5.0
-	remeshInterval = 10
+	remeshInterval = 20
 	useDirectRemesh = .TRUE.
 /
 
 &fileIO
 	outputDir = '${OUTPUT_LOC}'
-	outputRoot = 'divWindCosBells_directRemesh'
+	outputRoot = 'divGHills_directRemesh'
 	frameOut = 100
 /
 EOF
 
-mpirun -np 8 advectDivergentWind.exe divWindCosineBells.namelist 2>&1 | tee -a $JOB_FILENAME
+mpirun -np 8 advectGHillsDivWind.exe divWindGHills.namelist 2>&1 | tee -a $JOB_FILENAME
 
 done
 
