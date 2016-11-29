d7cc0a9417ec53dbe2bcd4866e371abda91be87b
Peter Bosler <pabosle@sandia.gov>
2016-11-29 12:13:54 -0700

    updated run scripts for timing experiment on skybridge.

--- runGaussianHillsDirect.sh
+++ runGaussianHillsDirect.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --time=0:60:00
+#SBATCH --nodes=4
+#SBATCH --time=0:18:00
 #SBATCH --account=fy150039
-#SBATCH --job-name=adv-gHills-lpmD
+#SBATCH --job-name=gHills-lpmD
 #SBATCH --partition=ec
 #SBATCH --mail-type=ALL
 
+module purge
+module load intel/15.0
+module load gnu/4.9.2
+module load openmpi-intel/1.8
+
+nnodes=$SLURM_JOB_NUM_NODES
+cores=16
+nnp=$(($cores*$nnodes))
+
 LPM_ROOT=$HOME/lpm-v2
 #OUTPUT_LOC=$HOME/modelData/transport
-OUTPUT_LOC=/fscratch/pabosle/modelData/transport
-JOB_FILENAME=gHillsDirectOutput.txt
+OUTPUT_LOC=/fscratch/pabosle/modelData/transport/timingDirect
+JOB_FILENAME=gHillsDirectOutput_np${nnp}.txt
 
-cd $LPM_ROOT/build
+cp $LPM_ROOT/build/advectGaussHillsSphere.exe $OUTPUT_LOC/.
+cd $OUTPUT_LOC
 
 rm -f $JOB_FILENAME
 
@@ -34,12 +44,12 @@ cat <<EOF > gHillsDirect.namelist
 
 &fileIO
 	outputDir = '${OUTPUT_LOC}'
-	outputRoot = 'gHills_directRemesh'
-	frameOut = 100
+	outputRoot = 'gHills_direct_np${nnp}'
+	frameOut = 400
 /
 EOF
 
-mpirun -np 8 advectGaussHillsSphere.exe gHillsDirect.namelist 2>&1 | tee -a $JOB_FILENAME
+mpirun --bind-to core --npernode $cores -np $nnp advectGaussHillsSphere.exe gHillsDirect.namelist 2>&1 | tee -a $OUTPUT_LOC/$JOB_FILENAME
 
 done
 
--- runGaussianHillsIndirect.sh
+++ runGaussianHillsIndirect.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --time=0:60:00
+#SBATCH --nodes=4
+#SBATCH --time=0:18:00
 #SBATCH --account=fy150039
-#SBATCH --job-name=adv-gHills-lpmI
+#SBATCH --job-name=gHills-lpmI
 #SBATCH --partition=ec
 #SBATCH --mail-type=ALL
 
+module purge
+module load intel/15.0
+module load gnu/4.9.2
+module load openmpi-intel/1.8
+
 LPM_ROOT=$HOME/lpm-v2
 #OUTPUT_LOC=$HOME/modelData/transport
-OUTPUT_LOC=/fscratch/pabosle/modelData/transport
-JOB_FILENAME=gHillsIndOutput.txt
+OUTPUT_LOC=/fscratch/pabosle/modelData/transport/timingIndirect
 
-cd $LPM_ROOT/build
+nnodes=$SLURM_JOB_NUM_NODES
+cores=16
+nnp=$(($cores*$nnodes))
+
+JOB_FILENAME=gHillsIndOutput_np${nnp}.txt
+
+cp $LPM_ROOT/build/advectGaussHillsSphere.exe $OUTPUT_LOC/.
+cd $OUTPUT_LOC
 
 rm -f $JOB_FILENAME
 
@@ -34,12 +45,12 @@ cat <<EOF > gHillsInd.namelist
 
 &fileIO
 	outputDir = '${OUTPUT_LOC}'
-	outputRoot = 'gHills_indirectRemesh'
-	frameOut = 100
+	outputRoot = 'gHills_indirect_np${nnp}'
+	frameOut = 400
 /
 EOF
 
-mpirun -np 8 advectGaussHillsSphere.exe gHillsInd.namelist 2>&1 | tee -a $JOB_FILENAME
+mpirun --bind-to core --npernode $cores -np $nnp advectGaussHillsSphere.exe gHillsInd.namelist 2>&1 | tee -a $OUTPUT_LOC/$JOB_FILENAME
 
 done
 
